b135c58c6947ff9fda38ae70e172f4f364dc6cfe
Mehedi Hasan <57999057+Moidda@users.noreply.github.com>
2022-02-17 20:40:19 +0600

    Request prescriptions can now be uploaded and viewed

--- Requester/createRequest/urls.py
+++ Requester/createRequest/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'createRequest'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('process/', views.processRequest, name='processRequest'),
 ]
--- Requester/createRequest/views.py
+++ Requester/createRequest/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, HttpResponseRedirect, FileResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -7,3 +8,16 @@ from django.shortcuts import render, get_object_or_404
 def index(request):
     context = {}
     return render(request, "createRequest/index.html", context)
+
+
+def processRequest(request):
+    name = request.POST.get('fname')
+    phone = request.POST.get('phone')
+    address = request.POST.get('city')
+    hospital_name = request.POST.get('hospital_name')
+    prescription = request.FILES.get('prescription')
+    fs = FileSystemStorage()
+    filename = fs.save(prescription.name, prescription)
+    uploaded_file_url = fs.url(filename)
+    response = 'File Upload Successful. Go to this link: ' + uploaded_file_url
+    return HttpResponse(response)
